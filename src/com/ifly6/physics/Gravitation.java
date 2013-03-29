6f7bcc7fff7e583969a18099d0fe5145a9083afa
ifly6 <kevinw26@gmail.com>
2013-03-29 01:55:34 -0400

    added methods on classical gravitation

---
+++ src/com/ifly6/physics/Gravitation.java
@@ -0,0 +1,56 @@
+package com.ifly6.physics;
+
+public class Gravitation {
+
+	/**
+	 * Calculates from the four criteria, the total gravitational attraction
+	 * between the two objects.
+	 * 
+	 * @param mass1
+	 *            - Mass of first object
+	 * @param mass2
+	 *            - Mass of second object
+	 * @param distance
+	 *            - distance between the two
+	 * @param gravConstant
+	 *            - the gravitational constant
+	 * @return
+	 */
+	public double newtonian(double mass1, double mass2, double distance,
+			double gravConstant) {
+		double newtons = gravConstant
+				* ((mass1 * mass2) / Math.pow(distance, 2));
+		return newtons;
+	}
+
+	/**
+	 * This is a caller for the other Newtonian gravitation class - just with
+	 * the gravitational constant hard-coded into the programme.
+	 * 
+	 * @param mass1
+	 *            - Mass of first object
+	 * @param mass2
+	 *            - Mass of second object
+	 * @param distance
+	 *            - distance between the two
+	 * @return
+	 * @see newtonian(double, double, double, double)
+	 */
+	public double newtonian(double mass1, double mass2, double distance) {
+		// Gravitational Constant = 6.6738480 E-11
+		double newtons = newtonian(mass1, mass2, distance, 6.6738480E-11);
+		return newtons;
+	}
+
+	/**
+	 * Calculate the Newtons of an object falling in standard Paris gravity.
+	 * 
+	 * @param mass
+	 *            - Mass of the object in question.
+	 * @return Amount of force of object, in Newtons.
+	 */
+	public double earth(double mass) {
+		return mass * 9.81;
+	}
+
+}
