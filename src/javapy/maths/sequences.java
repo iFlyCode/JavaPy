259ac5f6d1340a7ad7e6fcc3e8cf600faa201e3d
Nicholas Colaprete <ncolaprete@gmail.com>
2013-03-30 16:01:59 -0400

    Added Julia Set formula, begun Mandelbrot formula
    Mandelbrot formula is unfinished.

--- src/javapy/maths/sequences.java
+++ src/javapy/maths/sequences.java
@@ -57,4 +57,56 @@ public class Sequences {
 		}
 		return number;
 	}
+	
+	/**
+	 * Takes an imaginary number, and returns whether or not it is bounded under 2
+	 * after a certain number of iterations, by the equation z = z^2 + (second given imaginary number)
+	 * 
+	 * @author ncolaprete
+	 * @param real - Real part of the number to check if bounded
+	 * @param imag - Imaginary part of the number to check if bounded
+	 * @param cReal - Real part of the julia set to check in
+	 * @param cImag - Imaginary part of the julia set to check in
+	 * @param iters - Number of iterations to check if the number is bounded
+	 * @return number of iterations before the number became unbounded,
+	 *             - or returns the number of iterations given if is bounded
+	 */
+	public int julia(double real, double imag, double cReal, double cImag, int iters) {
+		int to_sender = 0;
+		for (int i=0;i<iters;i++) {
+			double backReal = real;
+			double backImag = imag;
+			real = ( (backReal*backReal) - (backImag*backImag) ) + cReal;
+			imag = ( 2 * backImag * backReal ) + cImag;
+			if ((real*real) + (imag*imag) > 4)
+				break;
+			to_sender = i+1;
+		}
+		return to_sender;
+	}
+	
+	/**
+	 * Takes an imaginary number, and returns whether or not it is bounded under 2
+	 * after a certain number of iterations, by the equation z = z^2 + c
+	 * 
+	 * @author ncolaprete
+	 * @param real - Real part of the number to check if bounded
+	 * @param imag - Imaginary part of the number to check if bounded
+	 * @param iters - Number of iterations to check if the number is bounded
+	 * @return number of iterations before the number became unbounded,
+	 *             - or returns the number of iterations given if is bounded
+	 */
+	public int mandelbrot(double real, double imag, int iters) { // TO FIX
+		int to_sender = 0;
+		for (int i=0;i<iters;i++) {
+			double backReal = real;
+			double backImag = imag;
+			real = ( (backReal*backReal) - (backImag*backImag) ) - real;
+			imag = ( 2 * backImag * backReal ) + imag;
+			if ((real*real) + (imag*imag) > 4)
+				break;
+			to_sender = i+1;
+		}
+		return to_sender;
+	}
 }
